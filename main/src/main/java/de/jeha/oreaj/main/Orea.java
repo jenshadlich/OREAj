458c1ddc7681c410a72d5623d87856a106095363
jenshadlich <jenshadlich@googlemail.com>
2015-01-30 21:07:38 +0100

    refactor

--- main/src/main/java/de/jeha/oreaj/main/Orea.java
+++ main/src/main/java/de/jeha/oreaj/main/Orea.java
@@ -23,7 +23,7 @@ public class Orea {
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String... args) {
         simpleTask2();
     }
 
