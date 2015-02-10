0c58993656cb6578c4fb5b8b0eb4e943898ce969
jenshadlich <jenshadlich@googlemail.com>
2015-02-10 17:39:35 +0100

    add test for shuffle automaton operation

---
+++ regex/src/test/java/de/jeha/oreaj/regex/ShuffleTest.java
@@ -0,0 +1,29 @@
+package de.jeha.oreaj.regex;
+
+import de.jeha.oreaj.regex.eval.AutomatonHelper;
+import de.jeha.oreaj.regex.eval.AutomatonHelperTest;
+import dk.brics.automaton.Automaton;
+import dk.brics.automaton.RegExp;
+import dk.brics.automaton.ShuffleOperations;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShuffleTest {
+
+    @Test
+    public void shuffle() {
+        // (aa)* $ b*
+        Automaton a = new RegExp("(aa)*").toAutomaton();
+        Automaton b = new RegExp("b*").toAutomaton();
+        Automaton shuffle = ShuffleOperations.shuffle(a, b);
+
+        List<String> accepted = AutomatonHelper.accepted(shuffle, 10);
+
+        System.out.println(accepted);
+
+        assertEquals("", accepted.get(0));
+    }
+}
