a0203e0c406730a262920c233294b35f8135286b
jenshadlich <jenshadlich@googlemail.com>
2015-02-10 17:37:50 +0100

    make public

--- regex/src/main/java/de/jeha/oreaj/regex/eval/AutomatonHelper.java
+++ regex/src/main/java/de/jeha/oreaj/regex/eval/AutomatonHelper.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-class AutomatonHelper {
+public class AutomatonHelper {
 
     /**
      * TODO: refactor to use iterator pattern
