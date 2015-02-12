2e9f05fc1f5e7ddbf4f4eac97d651b100cc2569e
jenshadlich <jenshadlich@googlemail.com>
2015-02-12 15:34:06 +0100

    add shuffle operation

---
+++ regex/src/main/java/de/jeha/oreaj/regex/rx/Shuffle.java
@@ -0,0 +1,16 @@
+package de.jeha.oreaj.regex.rx;
+
+public class Shuffle extends Op2 {
+
+    private final static String GLUE_STRING = "$";
+
+    public Shuffle(RX left, RX right) {
+        super(left, right);
+    }
+
+    @Override
+    protected String getGlueString() {
+        return GLUE_STRING;
+    }
+
+}
