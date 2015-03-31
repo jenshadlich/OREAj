eeb613b2683ecb6e60c27cb8f4c60d35f44b0d4c
jenshadlich <jenshadlich@googlemail.com>
2015-04-01 00:33:35 +0200

    add a random mutation that chooses randomly from other mutations

---
+++ regex/src/main/java/de/jeha/oreaj/regex/mutator/Random.java
@@ -0,0 +1,30 @@
+package de.jeha.oreaj.regex.mutator;
+
+import de.jeha.oreaj.genetic.Mutator;
+import de.jeha.oreaj.regex.rx.RX;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class Random implements Mutator<RX> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Random.class);
+
+    private static final java.util.Random GENERATOR = new java.util.Random();
+
+    private static final List<Mutator<RX>> MUTATORS = Arrays.asList(
+            new Clone()
+    );
+
+    @Override
+    public RX mutate(RX genotype) {
+        final Mutator<RX> mutator = MUTATORS.get(GENERATOR.nextInt(MUTATORS.size()));
+        LOG.debug("Chosen mutation: {}", mutator.getClass().getName());
+        return mutator.mutate(genotype);
+    }
+}
