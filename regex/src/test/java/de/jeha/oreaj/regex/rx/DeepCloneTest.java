79c014873431efd3c894003d45313da6a46c047a
jenshadlich <jenshadlich@googlemail.com>
2015-04-03 23:02:18 +0200

    attach test case

---
+++ regex/src/test/java/de/jeha/oreaj/regex/rx/DeepCloneTest.java
@@ -0,0 +1,21 @@
+package de.jeha.oreaj.regex.rx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public class DeepCloneTest {
+
+    @Test
+    public void test() {
+        RX rx = new Dot(new Star(new Letter("a")), new Letter("b"));
+        RX mutant = rx.deepClone();
+
+        assertEquals(rx.show(), mutant.show());
+        assertNotEquals(rx, mutant);
+    }
+}
