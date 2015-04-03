ae8ab5c407a3d58e8265b10bfc6af8917e73cf4e
jenshadlich <jenshadlich@googlemail.com>
2015-04-03 23:12:24 +0200

    javadoc

--- regex/src/main/java/de/jeha/oreaj/regex/rx/RX.java
+++ regex/src/main/java/de/jeha/oreaj/regex/rx/RX.java
@@ -2,12 +2,26 @@ package de.jeha.oreaj.regex.rx;
 
 import java.util.List;
 
+/**
+ * @author jenshadlich@googlemail.com
+ */
 public interface RX extends Iterable<RX> {
 
+    /**
+     * @return a string representation of the rx
+     */
     String show();
 
-	List<RX> siblings();
+    /**
+     * @return a list of all siblings (2 for Op2, 1 for Op1 or empty for terminal symbol Letter)
+     */
+    List<RX> siblings();
 
+    /**
+     * Make a deep copy of the current object.
+     *
+     * @return the identical clone
+     */
     RX deepClone();
 
 }
