9fb87c368edc720f9692f2c439d99f93e26f5072
jenshadlich <jenshadlich@googlemail.com>
2015-04-05 00:31:03 +0200

    attach test cases

---
+++ regex/src/test/java/de/jeha/oreaj/regex/mutation/PointMutationTest.java
@@ -0,0 +1,47 @@
+package de.jeha.oreaj.regex.mutation;
+
+import de.jeha.oreaj.regex.rx.Letter;
+import de.jeha.oreaj.regex.rx.RX;
+import de.jeha.oreaj.regex.rx.Star;
+import de.jeha.oreaj.regex.rx.Union;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PointMutationTest {
+
+    @Test
+    public void test() {
+        String[] sigma = {"a", "b", "c"};
+        RX rx = new Star(new Union(new Letter("a"), new Letter("b"))); // (a|b)*
+        RX mutant = new PointMutation(sigma).mutate(rx);
+
+        assertNotEquals(rx, mutant);
+        assertTrue(mutant.show().equals("(b|b)*")
+                || mutant.show().equals("(c|b)*")
+                || mutant.show().equals("(a|a)*")
+                || mutant.show().equals("(a|c)*"));
+    }
+
+    @Test
+    public void testWithRXBeingOnlyOneLetter() {
+        String[] sigma = {"a", "b"};
+        RX rx = new Letter("a");
+        RX mutant = new PointMutation(sigma).mutate(rx);
+
+        assertNotEquals(rx, mutant);
+        assertEquals(mutant.show(), new Letter("b").show());
+    }
+
+    @Test
+    public void testWithStarAndLetter() {
+        String[] sigma = {"a", "b"};
+        RX rx = new Star(new Letter("a"));
+        RX mutant = new PointMutation(sigma).mutate(rx);
+
+        assertNotEquals(rx, mutant);
+        assertEquals(mutant.show(), new Star(new Letter("b")).show());
+    }
+}
