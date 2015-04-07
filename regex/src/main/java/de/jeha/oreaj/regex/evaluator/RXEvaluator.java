4b7612e191ffd82a69d38810d49232607f79f175
jenshadlich <jenshadlich@googlemail.com>
2015-04-07 22:19:57 +0200

    add todo

--- regex/src/main/java/de/jeha/oreaj/regex/evaluator/RXEvaluator.java
+++ regex/src/main/java/de/jeha/oreaj/regex/evaluator/RXEvaluator.java
@@ -4,6 +4,9 @@ import de.jeha.oreaj.regex.rx.RX;
 import dk.brics.automaton.Automaton;
 import dk.brics.automaton.RegExp;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RXEvaluator extends AbstractRXEvaluator {
 
     private final Automaton target;
@@ -13,8 +16,8 @@ public class RXEvaluator extends AbstractRXEvaluator {
     }
 
     @Override
-    public double evaluate(RX individual) {
-        RegExp regExp = new RegExp(individual.show());
+    public double evaluate(RX rx) {
+        RegExp regExp = new RegExp(rx.show());
         Automaton semantics = regExp.toAutomaton();
         Automaton tooMuch = semantics.minus(target);
         Automaton missing = target.minus(semantics);
@@ -22,7 +25,20 @@ public class RXEvaluator extends AbstractRXEvaluator {
         double weightTooMuch = 100_000 * weight(tooMuch);
         double weightMissing = 1_000 * weight(missing);
 
+        // TODO: punish if rx has terminals that target does not
+
         return weightTooMuch + weightMissing;
     }
 
+    private Set<String> getTerminalSet(RX rx) {
+        Set<String> terminalSet = new HashSet<>();
+        final String rxString = rx.show();
+        for (int i = 0; i < rxString.length(); i++) {
+            char c = rxString.charAt(i);
+            if (Character.isLetter(c)) {
+                terminalSet.add(Character.toString(c));
+            }
+        }
+        return terminalSet;
+    }
 }
