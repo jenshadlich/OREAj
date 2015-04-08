73d027d1ddee327e94245c341204ac2f9c385954
jenshadlich <jenshadlich@googlemail.com>
2015-04-08 23:43:24 +0200

    add test cases

--- regex/src/test/java/de/jeha/oreaj/regex/evaluator/RXEvaluatorWithLengthTest.java
+++ regex/src/test/java/de/jeha/oreaj/regex/evaluator/RXEvaluatorWithLengthTest.java
@@ -1,6 +1,8 @@
 package de.jeha.oreaj.regex.evaluator;
 
 import de.jeha.oreaj.genetic.core.Evaluator;
+import de.jeha.oreaj.regex.parse.NoParseException;
+import de.jeha.oreaj.regex.parse.RegexParser;
 import de.jeha.oreaj.regex.rx.Letter;
 import de.jeha.oreaj.regex.rx.RX;
 import dk.brics.automaton.RegExp;
@@ -17,4 +19,22 @@ public class RXEvaluatorWithLengthTest {
 
         assertEquals(3_000.1, result, 1.0e-3);
     }
+
+    @Test
+    public void test2() throws NoParseException {
+        Evaluator<RX> evaluator = new RXEvaluatorWithLength(new RegExp("(aa|ba)*").toAutomaton());
+        double result = evaluator.evaluate(new RegexParser("((a)*)*").parse());
+
+        assertEquals(67_198.553, result, 1.0e-3);
+
+    }
+
+    @Test
+    public void test3() throws NoParseException {
+        Evaluator<RX> evaluator = new RXEvaluatorWithLength(new RegExp("(aa|ba)*").toAutomaton());
+        double result = evaluator.evaluate(new RegexParser("(a(a)*|(a)*)*").parse());
+
+        assertEquals(67_199.153, result, 1.0e-3);
+    }
+
 }
