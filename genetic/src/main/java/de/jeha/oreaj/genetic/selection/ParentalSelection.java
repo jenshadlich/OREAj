5d5a59173338544c2ce5c153505d07567e7ff32f
jenshadlich <jenshadlich@googlemail.com>
2015-04-08 17:22:51 +0200

    doc, add todo

--- genetic/src/main/java/de/jeha/oreaj/genetic/selection/ParentalSelection.java
+++ genetic/src/main/java/de/jeha/oreaj/genetic/selection/ParentalSelection.java
@@ -4,6 +4,11 @@ import de.jeha.oreaj.genetic.core.Population;
 
 import java.util.List;
 
+/**
+ * TODO: Refactor; currently more used for both selection AND (parental) variation.
+ *
+ * @param <GT> genotype
+ */
 public interface ParentalSelection<GT> {
 
     List<GT> select(Population<GT> population);
